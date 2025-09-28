33139bbce60f383d733d988fed95c868038e7488
Dinis Saraiva <dinis.saraiva.santos@gmail.com>
2025-09-28 23:31:27 +0100

    Add Header component and react-router-dom dependency
    Introduced a new Header component with navigation and user context integration. Added react-router-dom to package.json for routing support.

---
+++ tv-show-tracker/src/Header.js
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import { UserContext } from './UserContext';
+import { Button, Navbar, Nav, Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+  const { user, handleLogout } = useContext(UserContext);
+
+  return (
+    <Navbar bg="dark" variant="dark" expand="lg">
+      <Container>
+        <Navbar.Brand as={Link} to="/">MyApp</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/tvshows">TV Shows</Nav.Link>
+          </Nav>
+          <Nav>
+            {user ? (
+              <>
+                <Navbar.Text className="me-2">Signed in as: {user.name}</Navbar.Text>
+                <Button variant="outline-light" onClick={handleLogout}>Logout</Button>
+              </>
+            ) : (
+              <>
+                <Nav.Link as={Link} to="/login">Login</Nav.Link>
+                <Nav.Link as={Link} to="/register">Register</Nav.Link>
+              </>
+            )}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default Header;
