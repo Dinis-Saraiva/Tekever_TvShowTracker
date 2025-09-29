8b0bdefe6ea8e2057a7067dc674902965850852d
Dinis Saraiva <dinis.saraiva.santos@gmail.com>
2025-09-29 18:24:07 +0100

    Update queries.js

--- tv-show-tracker/src/queries.js
+++ tv-show-tracker/src/queries.js
@@ -6,8 +6,12 @@ export const GET_TVSHOWS_PAGINATED = gql`
     $after: String
     $last: Int
     $before: String
+    $where: TvShowFilterInput
+    $order: [TvShowSortInput!]
+    
   ) {
-    tvShows(first: $first, after: $after, last: $last, before: $before) {
+    tvShows(first: $first, after: $after, last: $last, before: $before,where: $where,
+      order: $order) {
       edges {
         node {
           id
@@ -35,10 +39,8 @@ export const GET_TVSHOWS_PAGINATED = gql`
 `;
 
 export const GET_TVSHOW_BY_ID = gql`
-  query GetTvShows($id: ID!) {
-    tvShows(id: $id) {
-    edges {
-        node {
+  query GetTvShowById($id: Int!) {
+    tvShowById(id: $id) {
       id
       name
       description
@@ -47,9 +49,54 @@ export const GET_TVSHOW_BY_ID = gql`
       rating
       imageUrl
       origin
+      rating
       genres {
         name
       }
+      actors { id name
+        }
+      directors { id name
+        }
     }
-}}}
+  }
+`;
+
+export const GET_EPISODES_BY_TVSHOW_ID = gql`
+  query GetEpisodesByTvShowId($tvShowId: Int!, $first: Int, $after: String, $last: Int, $before: String) {
+    episodesByTvShowId(tvShowId: $tvShowId, first: $first, after: $after, last: $last, before: $before) {
+      edges {
+        node {
+          id
+          title
+          airDate
+          summary
+          seasonNumber
+          episodeNumber
+        }
+        cursor
+      }
+      pageInfo {
+        hasNextPage
+        hasPreviousPage
+        startCursor
+        endCursor
+      }
+    }
+  }
+`;
+
+export const GET_PERSON_BY_PERSONID = gql`
+  query GetPersonById($id: Int!) {
+    personById(id: $id) {
+      id
+      name
+      bio
+      birthDate
+      profileImageUrl
+      workedOn { id role
+        tvShow { id name
+        }
+      }
+    }
+  }
 `;
