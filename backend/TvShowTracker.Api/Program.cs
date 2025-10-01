3c93530baa82f046dcc95d23cf16af4ea7661659
Dinis Saraiva <dinis.saraiva.santos@gmail.com>
2025-10-01 23:53:24 +0100

    Added EnvVariables for backend

--- backend/TvShowTracker.Api/Program.cs
+++ backend/TvShowTracker.Api/Program.cs
@@ -4,25 +4,28 @@ using TvShowTracker.Api.Data;
 using TvShowTracker.Api.GraphQL;
 using TvShowTracker.Api.Models;
 
+// Load .env file from project root
+DotNetEnv.Env.Load(System.IO.Path.Combine(AppContext.BaseDirectory, "..\\..\\..\\.env"));
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
-/* builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"))); */
 
+// Database
+var defaultConnection = Environment.GetEnvironmentVariable("DEFAULT_CONNECTION")
+                        ?? throw new InvalidOperationException("Missing DEFAULT_CONNECTION in .env");
 builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlite(defaultConnection));
 
+// Identity
 builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
     .AddEntityFrameworkStores<ApplicationDbContext>()
     .AddDefaultTokenProviders();
 
-
 builder.Services.AddMemoryCache();
 
+// Configure cookies
 builder.Services.ConfigureApplicationCookie(options =>
 {
     options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
@@ -33,21 +36,20 @@ builder.Services.ConfigureApplicationCookie(options =>
     options.Cookie.SecurePolicy = CookieSecurePolicy.Always;
 });
 
-// Add CORS policy
+// CORS
+var frontendUrl = Environment.GetEnvironmentVariable("FRONTEND_URL") ?? "http://localhost:3000";
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowFrontend",
-        policy =>
-        {
-            policy.WithOrigins("http://localhost:3000") // your frontend dev server
-                  .AllowAnyHeader()
-                  .AllowAnyMethod()
-                  .AllowCredentials();
-        });
+    options.AddPolicy("AllowFrontend", policy =>
+    {
+        policy.WithOrigins(frontendUrl)
+              .AllowAnyHeader()
+              .AllowAnyMethod()
+              .AllowCredentials();
+    });
 });
 
-
-
+// Controllers and GraphQL
 builder.Services.AddControllers();
 builder.Services
     .AddGraphQLServer()
@@ -55,56 +57,46 @@ builder.Services
     .AddDataLoader<GenresByTvShowIdDataLoader>()
     .AddType<TvShowType>()
     .AddType<ObjectType<Genre>>()
-    .ModifyCostOptions(opt =>
-    {
-        opt.MaxFieldCost = 4000;
-    })
+    .ModifyCostOptions(opt => opt.MaxFieldCost = 4000)
     .AddFiltering()
     .AddSorting()
     .AddProjections();
-    
 
-builder.Services.AddScoped<IEmailService, EmailService>();
+// EmailService using SendGrid
+builder.Services.AddScoped<IEmailService>(sp =>
+{
+    var sendGridApiKey = Environment.GetEnvironmentVariable("SENDGRID_API_KEY")
+        ?? throw new InvalidOperationException("Missing SENDGRID_API_KEY in .env");
+    var fromEmail = Environment.GetEnvironmentVariable("FROM_EMAIL")
+        ?? throw new InvalidOperationException("Missing FROM_EMAIL in .env");
+    var fromName = Environment.GetEnvironmentVariable("FROM_NAME") ?? "No-Reply";
+
+    return new EmailService(sendGridApiKey, fromEmail, fromName);
+});
+
+// Recommendation service
 builder.Services.AddScoped<RecommendationService>();
 builder.Services.AddHostedService<RecommendationWorker>();
 
 var app = builder.Build();
-// Configure the HTTP request pipeline.CsvImporter.ImportTvShows("data.csv", db);
+
+// OpenAPI in Development
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
 }
 
-/* using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    db.Database.EnsureCreated();
-    TvShowVectorCalculator.CalculateVectors(db);
-    CsvImporter.ImportTvShows("tvshows.csv", db);
- 
-} */
-
-/*  using (var scope = app.Services.CreateScope())
-{
-    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-
-    // Make sure the database is created
-    db.Database.EnsureCreated();
-
-    // Seed episodes
-    PersonSeeder.SeedPeople(db);
-}
-  */
-
+// Middleware
 app.UseCors("AllowFrontend");
 app.UseAuthentication();
 app.UseAuthorization();
 app.UseStaticFiles();
-
 app.UseHttpsRedirection();
+
+// Endpoints
 app.MapControllers();
 app.MapGraphQL();
+
 app.Run();
 
 public partial class Program { }
-
